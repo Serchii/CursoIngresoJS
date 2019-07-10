6cb159760b0269b1d29dab13e42c101a363555de
Serchii <52494547+Serchii@users.noreply.github.com>
2019-07-10 12:23:06 -0300

    Clase 3

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,29 @@
+/*Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4*/
+
+
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var nota;
+
+	nota = Math.floor((Math.random()*10)+1);
+
+	if (nota > 8){
+
+		alert("Nota: "+nota+"\nEXCELENTE");
+
+	} else if (nota > 3) {
+
+		alert("Nota: "+nota+"\nAPROBÓ");
+
+	} else {
+
+		alert("Nota: "+nota+"\nVamos, la proxima se puede");
+
+	}
 
 }//FIN DE LA FUNCIÓN
