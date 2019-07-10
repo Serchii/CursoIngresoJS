70e27cb12421abee9b5fa8ba560e7799b294dce3
Serchii <52494547+Serchii@users.noreply.github.com>
2019-07-10 12:23:45 -0300

    Clase 3 Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,8 +1,30 @@
+/*al seleccionar un mes informar. 
+si es Enero: "que comiences bien el año!!!." 
+si es Marzo: "a clases!!!." 
+si es Julio: "se vienen las vacaciones!!!." 
+si es Diciembre: "Felices fiesta!!!."*/
+
+
 function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño){
+
+    case "Enero":
+        alert("Que comiences bien el año!!!");
+        break;
+    case "Marzo":
+        alert("A clases!!!");
+        break;
+    case "Julio":
+        alert("Se vienen las vacaciones!!!");
+        break;
+    case "Diciembre":
+        alert("Felices fiestas!!!");
+        break;
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,20 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch (mesDelAño){
+        case "Febrero":
+            alert("Tiene 28 dias");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("Tiene 30 dias");
+            break;
+        default:
+            alert("Tiene 31 dias");
+            break;
+    }
 
 
 
